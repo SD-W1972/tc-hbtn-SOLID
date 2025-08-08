d402e1930ffaec7b52cb0d047af74d34d263f39d
SD-W1972 <pernalongaesnobe@gmail.com>
2025-08-08 20:09:23 -0300

    feat: classe Logger refatorada com princípios clean code e conveção de nomenclatura Java

--- 0x04/log/LoggerInFile.java
+++ 0x04/log/LoggerInFile.java
@@ -1,31 +1,19 @@
-
 import java.io.IOException;
-
 import java.util.logging.FileHandler;
-
 import java.util.logging.Logger;
-
 import java.util.logging.SimpleFormatter;
 
 public class LoggerInFile {
 
     public static void main(String[] args) {
-
         Logger logger = Logger.getLogger("MyLog");
-
         FileHandler fileHandler;
-
         fileHandler = new FileHandler("logs.txt");
-
-        Logger.AddHandler(fileHandler);
-
-        simpleFormatter simpleFormatter = new SimpleFormatter();
-
+        logger.addHandler(fileHandler);
+        SimpleFormatter simpleFormatter = new SimpleFormatter();
         fileHandler.setFormatter(simpleFormatter);
-
-        Logger.info("teste de Log");
-
-        Logger.info("Olá, eu sou o teste da classe principal");
+        logger.info("teste de Log");
+        logger.info("Olá, eu sou o teste da classe principal");
 
     }
 
