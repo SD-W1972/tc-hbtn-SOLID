aa6e9aaa3cba043292006c7dedc1abd3767aefbc
SD-W1972 <pernalongaesnobe@gmail.com>
2025-08-08 19:52:41 -0300

    feat: classe Error refatorada com princípios clean code

--- 0x04/error/Error.java
+++ 0x04/error/Error.java
@@ -14,26 +14,8 @@ public class Error {
         resolveGroup(user);
         return userRepository.save(user);
     }
-    //B
-    public Object createUser(User user){
-
-        if(loadUser(user.getId()) instanceof User){
-            // user already exists
-            return USER_ALREADY_EXISTS; // error flag
-            return 101; // or error code
-        };
-
-        resolveGroup(user);
-        return userRepository.save(user);
-    }
 
     // Exceções com contexto
-    //A
-    public void validate(String id){
-        if(ownerRepo.count(id) == 0){
-            throw new ValidationException("ocorreu uma exceção ...")
-        }
-    }
     // B
     public void validate(String id){
         if(ownerRepo.count(id) == 0){
@@ -43,17 +25,6 @@ public class Error {
 
 
     // Não retorne null e não passe null
-    //A
-    public User createUser(User user){
-
-        if(loadUser(user.getId()) instanceof User){
-            // user already exists
-            return null;
-        };
-
-        resolveGroup(user);
-        return userRepository.save(user);
-    }
     //B
     public User createUser(User user){
 
