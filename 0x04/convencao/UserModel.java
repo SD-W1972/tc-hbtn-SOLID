aadf1a24605af03e2f4fd3d0558dbae67e4aa1c9
SD-W1972 <pernalongaesnobe@gmail.com>
2025-08-08 20:03:27 -0300

    feat: refatorando de novo, removendo espaços desnecessários

--- 0x04/convencao/UserModel.java
+++ 0x04/convencao/UserModel.java
@@ -1,13 +1,7 @@
 // classe users, representando nossa entidade
-
 class UserModel {
-
     int idUser;
-
     String descricao;
-
     final static int IDADE_MINIMA = 18;
-
     public GetUsers() {return [];}
-
 }
